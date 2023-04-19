1854f0e51eff59b7fb9b1ca8f72c6e93ce0bdda1
jamarin <joseantonio.marin@seresco.es>
2023-04-19 10:27:09 +0200

    Cambios para añadir un estado compuesto: · contador.state.ts: Se crea la interface ContadorState con el contador. Se crea el inicialState para asignarle un valor inicial · contador.reducer.ts: Se elimina el estado incial que ya teníamos y se importal el que acabamos de crear. Se adaptan los "on" para el nuevo estado

--- frontend/src/app/contador/contador.reducer.ts
+++ frontend/src/app/contador/contador.reducer.ts
@@ -1,14 +1,13 @@
 import { createReducer, on } from '@ngrx/store';
 import * as contadorActions from './contador.actions';
-
-export const initialState = 10;
+import { initialState } from './contador.state';
 
 export const contadorReducer = createReducer(
     initialState,
-    on(contadorActions.IncrementarAction, state => state + 1),
-    on(contadorActions.DecrementarAction, state => state - 1),
-    on(contadorActions.DuplicarAction, state => state * 2),
-    on(contadorActions.DemediarAction, state => state / 2),
-    on(contadorActions.ResetAction, state => 0),
-    on(contadorActions.MultiplicarPorFactorAction, (state, { payload }) => state * payload)
+    on(contadorActions.IncrementarAction, state => ({ ...state, contador: state.contador + 1 })),
+    on(contadorActions.DecrementarAction, state => ({ ...state, contador: state.contador - 1 })),
+    on(contadorActions.DuplicarAction, state => ({ ...state, contador: state.contador * 2 })),
+    on(contadorActions.DemediarAction, state => ({ ...state, contador: state.contador / 2 })),
+    on(contadorActions.ResetAction, state => ({ ...state, contador: 0 })),
+    on(contadorActions.MultiplicarPorFactorAction, (state, { payload }) => ({ ...state, contador: state.contador * payload }))
 );
---
+++ frontend/src/app/contador/contador.state.ts
@@ -0,0 +1,7 @@
+export interface ContadorState {
+    contador: number;
+}
+
+export const initialState: ContadorState = {
+    contador: 10
+};
